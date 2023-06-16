b61795cbe41d90738cf6d58d30edd8f2855950ca
shane-co <seamuss.mck@gmail.com>
2023-06-16 16:51:27 -0700

    Fixed versioning issue

--- app/src/main/java/pro/selecto/slothos/ui/HomeScreen.kt
+++ app/src/main/java/pro/selecto/slothos/ui/HomeScreen.kt
@@ -1,11 +1,14 @@
 package pro.selecto.slothos.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun HomeScreen(
@@ -15,7 +18,7 @@ fun HomeScreen(
     Column() {
         Text(
             text = "Checking",
-            style = MaterialTheme.typography.headlineSmall
+            style = MaterialTheme.typography.headlineSmall,
         )
         Button(
             onClick = onButtonClicked,
