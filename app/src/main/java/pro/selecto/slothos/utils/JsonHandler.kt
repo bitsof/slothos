bacf6de084d71afa791284aa0475d74e4a0c44de
shane-co <seamuss.mck@gmail.com>
2023-12-31 04:39:51 -0800

    Add functions to read and write from json

--- app/src/main/java/pro/selecto/slothos/utils/JsonHandler.kt
+++ app/src/main/java/pro/selecto/slothos/utils/JsonHandler.kt
@@ -1,5 +1,6 @@
 package pro.selecto.slothos.utils
 
+import android.content.Context
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
@@ -11,81 +12,139 @@ import pro.selecto.slothos.data.entities.Level
 import pro.selecto.slothos.data.entities.Mechanic
 import pro.selecto.slothos.data.entities.Muscle
 import java.io.File
+import java.io.FileNotFoundException
+import java.io.IOException
 import java.net.URL
+import java.nio.charset.Charset
 
-fun generateJsonFiles(): Unit {
-    // use json files to generate entries for Category, Equipment, Force, Level, Mechanic, Muscle, Tag
-    // insert those entries
-    // Category
-    val categoryNames: Array<String> = arrayOf("powerlifting", "strength", "stretching", "cardio", "olympic weightlifting", "strongman", "plyometrics")
-    val categoryList = mutableListOf<Category>()
-    for(name in categoryNames){
-        categoryList.add(Category(name = name, description = ""))
-    }
-    writeToAsset(serializableList = categoryList, jsonFileName = "category")
+class JsonHandler(private val context: Context) {
+    lateinit var rawExercises: List<RawExercise>
 
-    // Equipment
-    val equipmentNames: Array<String> = arrayOf("medicine ball", "dumbbell", "body only", "bands", "kettlebells", "foam roll", "cable", "machine", "barbell", "exercise ball", "e-z curl bar", "other")
-    val equipmentList = mutableListOf<Equipment>()
-    for(name in equipmentNames){
-        equipmentList.add(Equipment(name = name, description = ""))
-    }
-    writeToAsset(serializableList = equipmentList, jsonFileName = "equipment")
+    fun makeJsonFiles(): Unit {
+        // use json files to generate entries for Category, Equipment, Force, Level, Mechanic, Muscle, Tag
+        // insert those entries
+        // Category
+        val categoryNames: Array<String> = arrayOf("powerlifting", "strength", "stretching", "cardio", "olympic weightlifting", "strongman", "plyometrics")
+        val categoryList = mutableListOf<Category>()
+        for(name in categoryNames){
+            categoryList.add(Category(name = name, description = ""))
+        }
+        writeToAsset(serializableList = categoryList, jsonFileName = "category")
 
-    // Force
-    val forceNames: Array<String> = arrayOf("static", "pull", "push")
-    val forceList = mutableListOf<Force>()
-    for(name in forceNames){
-        forceList.add(Force(name = name, description = ""))
-    }
-    writeToAsset(serializableList = forceList, jsonFileName = "force")
+        // Equipment
+        val equipmentNames: Array<String> = arrayOf("medicine ball", "dumbbell", "body only", "bands", "kettlebells", "foam roll", "cable", "machine", "barbell", "exercise ball", "e-z curl bar", "other")
+        val equipmentList = mutableListOf<Equipment>()
+        for(name in equipmentNames){
+            equipmentList.add(Equipment(name = name, description = ""))
+        }
+        writeToAsset(serializableList = equipmentList, jsonFileName = "equipment")
 
-    // Level
-    val levelNames: Array<String> = arrayOf("beginner", "intermediate", "expert")
-    val levelList = mutableListOf<Level>()
-    for(name in levelNames){
-        levelList.add(Level(name = name))
-    }
-    writeToAsset(serializableList = levelList, jsonFileName = "level")
+        // Force
+        val forceNames: Array<String> = arrayOf("static", "pull", "push")
+        val forceList = mutableListOf<Force>()
+        for(name in forceNames){
+            forceList.add(Force(name = name, description = ""))
+        }
+        writeToAsset(serializableList = forceList, jsonFileName = "force")
 
-    // Mechanic
-    val mechanicNames: Array<String> = arrayOf("isolation", "compound",)
-    val mechanicList = mutableListOf<Mechanic>()
-    for(name in mechanicNames){
-        mechanicList.add(Mechanic(name = name, description = ""))
-    }
-    writeToAsset(serializableList = mechanicList, jsonFileName = "mechanic")
+        // Level
+        val levelNames: Array<String> = arrayOf("beginner", "intermediate", "expert")
+        val levelList = mutableListOf<Level>()
+        for(name in levelNames){
+            levelList.add(Level(name = name))
+        }
+        writeToAsset(serializableList = levelList, jsonFileName = "level")
 
-    // Muscle
-    val muscleNames: Array<String> = arrayOf("abdominals", "abductors", "adductors", "biceps", "calves", "chest", "forearms", "glutes", "hamstrings", "lats", "lower back", "middle back", "neck", "quadriceps", "shoulders", "traps", "triceps")
-    val muscleList = mutableListOf<Muscle>()
-    for(name in muscleNames){
-        muscleList.add(Muscle(name = name, description = ""))
+        // Mechanic
+        val mechanicNames: Array<String> = arrayOf("isolation", "compound",)
+        val mechanicList = mutableListOf<Mechanic>()
+        for(name in mechanicNames){
+            mechanicList.add(Mechanic(name = name, description = ""))
+        }
+        writeToAsset(serializableList = mechanicList, jsonFileName = "mechanic")
+
+        // Muscle
+        val muscleNames: Array<String> = arrayOf("abdominals", "abductors", "adductors", "biceps", "calves", "chest", "forearms", "glutes", "hamstrings", "lats", "lower back", "middle back", "neck", "quadriceps", "shoulders", "traps", "triceps")
+        val muscleList = mutableListOf<Muscle>()
+        for(name in muscleNames){
+            muscleList.add(Muscle(name = name, description = ""))
+        }
+        writeToAsset(serializableList = muscleList, jsonFileName = "muscle")
     }
-    writeToAsset(serializableList = muscleList, jsonFileName = "muscle")
+
+
+    inline fun <reified T> getData(context: Context, jsonFileName: String) : List<T> {
+            val assetManager = context.assets
+            try {
+                val format = Json { ignoreUnknownKeys = true }
+                val inputStream = assetManager.open("json/$jsonFileName.json")
+                val size = inputStream.available()
+                val buffer = ByteArray(size)
+                inputStream.read(buffer)
+                inputStream.close()
+                return format.decodeFromString<List<T>>(String(buffer, Charset.defaultCharset()))
+            } catch (e: IOException) {
+                throw FileNotFoundException("File json/$jsonFileName.json not found in assets")
+            }
+
+    }
+
+    fun getRawExerciseData(): List<RawExercise> {
+        // Tag
+        val tagNames: Array<String> = arrayOf("Default")
+
+        // gets json data from github
+        val rawJson: String = URL("https://raw.githubusercontent.com/yuhonas/free-exercise-db/main/dist/exercises.json").readText()
+        rawExercises = Json.decodeFromString<List<RawExercise>>(rawJson)
+        /*
+        val data = Json.decodeFromString<List<RawExercise>>(rawJson)
+        val exerciseList = mutableListOf<Exercise>()
+
+        for (element in data) {
+            // add exercise to list of exercises
+            exerciseList.add(Exercise(nameId = element.id, name = element.name, instructions = element.instructions.joinToString()))
+            // use json data and queries to build fk relationships
+            // iterate through all of category, equipment, force, level, mechanic, muscle, tag
+            // build fk relationship
+        } */
+
+        return rawExercises
+    }
+
+    fun createForeignKeyPair(
+        rawExercise: RawExercise,
+        name: String
+    ){
+
+    }
+
+    private inline fun <reified T> writeToAsset(
+        serializableList: List<T>,
+        jsonFileName: String,
+    ) : Unit {
+        val jsonAssetPath = "./app/src/main/assets/json/"
+        val format = Json{ prettyPrint = true }
+        val file = File("$jsonAssetPath$jsonFileName.json")
+        file.createNewFile()
+        println(format.encodeToString(serializableList))
+        file.writeText(format.encodeToString(serializableList))
+        return
+    }
+
+    private inline fun <reified T> readFromAsset(jsonFileName: String): List<T> {
+        val jsonAssetPath = "./json/"
+        val format = Json { ignoreUnknownKeys = true }
+        val file = File("$jsonAssetPath$jsonFileName.json")
+
+        // Ensure the file exists before reading
+        if (!file.exists()) {
+            throw FileNotFoundException("File $jsonAssetPath$jsonFileName.json not found in assets")
+        }
+        val fileContent = file.readText()
+        return format.decodeFromString<List<T>>(fileContent)
+    }
 }
 
-fun getData(): List<Exercise> {
-    // Tag
-    val tagNames: Array<String> = arrayOf("Default")
-
-    // gets json data from github
-    val rawJson: String = URL("https://raw.githubusercontent.com/yuhonas/free-exercise-db/main/dist/exercises.json").readText()
-    val data = Json.decodeFromString<List<RawExercise>>(rawJson)
-    val exerciseList = mutableListOf<Exercise>()
-
-    for (element in data) {
-        // add exercise to list of exercises
-        exerciseList.add(Exercise(nameId = element.id, name = element.name, instructions = element.instructions.joinToString()))
-
-        // use json data and queries to build fk relationships
-        // iterate through all of category, equipment, force, level, mechanic, muscle, tag
-        // build fk relationship
-
-    }
-    println("Initial exercise count: " + exerciseList.size)
-    return exerciseList
-}
 
 // Data class and enums representing the schema of raw exercise database
 // Defined here : https://github.com/yuhonas/free-exercise-db/blob/main/schema.json
@@ -106,22 +165,3 @@ data class RawExercise(
     val images: List<String>,
 )
 
-class JsonGenerator {
-}
-
-fun main() {
-    generateJsonFiles()
-}
-
-inline fun <reified T> writeToAsset(
-    serializableList: List<T>,
-    jsonFileName: String,
-) : Unit {
-    val jsonAssetPath = "./app/src/main/assets/json/"
-    val format = Json{ prettyPrint = true }
-    val file = File("$jsonAssetPath$jsonFileName.json")
-    file.createNewFile()
-    println(format.encodeToString(serializableList))
-    file.writeText(format.encodeToString(serializableList))
-    return
-}
