dffbc70bfd5c960351c539774514c033bc0e26fa
shane-co <seamuss.mck@gmail.com>
2024-01-03 01:44:58 -0800

    Add outline of filter menu

--- app/src/main/java/pro/selecto/slothos/ui/exercise/ExerciseListScreen.kt
+++ app/src/main/java/pro/selecto/slothos/ui/exercise/ExerciseListScreen.kt
@@ -2,17 +2,25 @@ package pro.selecto.slothos.ui.exercise
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.compose.viewModel
 import pro.selecto.slothos.R
@@ -48,11 +56,43 @@ fun ExerciseListScreen(
 
 @Composable
 fun ExerciseList(modifier: Modifier, exerciseList: List<ExerciseDetails>?) {
-    LazyColumn(modifier = modifier) {
+    var showFilterMenu by remember { mutableStateOf(false) }
+    Column() {
+        Text(
+            text = "Exercises",
+            style = MaterialTheme.typography.headlineSmall,
+        )
+        Button(onClick = { showFilterMenu = true }) {
+            Text("Filter")
+        }
+        if (showFilterMenu) {
+            FilterMenu(
+                onDismiss = { showFilterMenu = false },
+                onApplyFilter = { /* Apply filter logic */ }
+            )
+        }
+        LazyColumn(modifier = modifier) {
 
-        if (exerciseList?.count() != null) {
-            items(exerciseList.count()) { index ->
-                ExerciseItem(exerciseDetails = exerciseList[index])
+            if (exerciseList?.count() != null) {
+                items(exerciseList.count()) { index ->
+                    ExerciseItem(exerciseDetails = exerciseList[index])
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun FilterMenu(onDismiss: () -> Unit, onApplyFilter: (FilterOptions) -> Unit) {
+    Dialog(onDismissRequest = onDismiss) {
+        Column() {
+            Text("Select Filters", style = MaterialTheme.typography.bodySmall)
+            Button(onClick = {
+                // Call onApplyFilter with the selected filter options
+                onApplyFilter(FilterOptions(/*...*/))
+                onDismiss()
+            }) {
+                Text("Apply Filters")
             }
         }
     }
@@ -88,7 +128,7 @@ object ExerciseListProvider {
             mutableListOf(category),
             mutableListOf(equipment)
         )
-        var list = mutableListOf<ExerciseDetails>(
+        val list = mutableListOf<ExerciseDetails>(
             ExerciseDetails(
                 exercise = exercise1,
                 mutableListOf(category),
