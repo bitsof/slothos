a74905d16cf70e99946b4d3d7f753191eb079f22
shane-mckenzie <seamuss.mck@gmail.com>
2024-01-03 19:11:05 -0800

    Add category filter to exercise list (#20)
    * Change list to not require view model to be directly passed, due to how previews work
    
    * Add an object to provide preview with list
    
    * Add filter options data class
    
    * Change preview to have more entries
    
    * Add outline of filter menu
    
    * Add filter menu and category selection
    
    * Add method of filtering to view model
    
    * Improve null safety by adding empty string if expression is null

--- app/src/main/java/pro/selecto/slothos/ui/exercise/ExerciseListScreen.kt
+++ app/src/main/java/pro/selecto/slothos/ui/exercise/ExerciseListScreen.kt
@@ -1,21 +1,37 @@
 package pro.selecto.slothos.ui.exercise
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.compose.viewModel
 import pro.selecto.slothos.R
 import pro.selecto.slothos.data.ExerciseDetails
+import pro.selecto.slothos.data.entities.Category
+import pro.selecto.slothos.data.entities.Equipment
+import pro.selecto.slothos.data.entities.Exercise
 import pro.selecto.slothos.ui.navigation.NavigationDestination
 
 object ExerciseListDestination : NavigationDestination {
@@ -28,29 +44,119 @@ fun ExerciseListScreen(
     modifier: Modifier = Modifier,
     viewModelFactory: ViewModelProvider.Factory
 ) {
-    val viewModel: ExerciseListViewModel = viewModel(
-        factory = viewModelFactory
-    )
+    val viewModel: ExerciseListViewModel = viewModel(factory = viewModelFactory)
     val coroutineScope = rememberCoroutineScope()
 
-    val uiState by viewModel.uiState.collectAsState(initial = ExerciseListUiState(emptyList()))
-    val listSize = uiState?.exerciseDetailsList?.count()
+    val uiState by viewModel.uiState.collectAsState()
+    var showCategoryDialog by remember { mutableStateOf(false) }
 
-    LazyColumn(modifier = modifier) {
-        if (listSize != null) {
-            items(listSize) { index ->
-                uiState?.exerciseDetailsList?.get(index)?.let { ExerciseItem(exerciseDetails = it) }
+    // Assuming viewModel provides filter options and categories
+    val filterOptions by viewModel.filterOptions.collectAsState()
+    val categories = uiState?.categoryList?: emptyList<Category>()
+
+    ExerciseList(
+        modifier = modifier,
+        exerciseList = uiState?.exerciseDetailsList,
+        categories = categories,
+        filterOptions = filterOptions
+    ) { newFilterOptions ->
+        viewModel.updateFilterOptions(newFilterOptions)
+    }
+
+
+}
+
+@Composable
+fun ExerciseList(
+    modifier: Modifier,
+    exerciseList: List<ExerciseDetails>?,
+    categories: List<Category>?,
+    filterOptions: FilterOptions,
+    onFilterChange: (FilterOptions) -> Unit
+) {
+    var showFilterMenu by remember { mutableStateOf(false) }
+
+    Column {
+        Text(
+            text = "Exercises",
+            style = MaterialTheme.typography.headlineSmall
+        )
+        Button(onClick = { showFilterMenu = true }) {
+            Text("Filter")
+        }
+        if (showFilterMenu) {
+            FilterMenu(
+                categories = categories,
+                filterOptions = filterOptions,
+                onDismiss = { showFilterMenu = false },
+                onApplyFilter = onFilterChange,
+                onCategorySelected = { categoryId ->
+                    onFilterChange(filterOptions.copy(category = categoryId))
+                }
+            )
+        }
+
+        LazyColumn(modifier = modifier) {
+            exerciseList?.let { list ->
+                items(list.count()) { index ->
+                    val exerciseDetails = list[index]
+                    ExerciseItem(exerciseDetails = exerciseDetails)
+                }
             }
         }
     }
 }
 
+
+@Composable
+fun FilterMenu(
+    categories: List<Category>?,
+    filterOptions: FilterOptions,
+    onDismiss: () -> Unit,
+    onApplyFilter: (FilterOptions) -> Unit,
+    onCategorySelected: (Int) -> Unit
+) {
+    var showCategoryDialog by remember { mutableStateOf(false) }
+    Dialog(onDismissRequest = onDismiss) {
+        Column() {
+            Text("Select Filters", style = MaterialTheme.typography.bodySmall)
+
+            // Button to open category selection dialog
+            Button(onClick = { showCategoryDialog = true }) {
+                Text("Select Category")
+            }
+
+            // Category Selection Dialog
+            if (showCategoryDialog && categories != null) {
+                CategorySelectionDialog(
+                    categories = categories,
+                    selectedCategoryId = filterOptions.category,
+                    onCategorySelected = {
+                        onCategorySelected(it)
+                        showCategoryDialog = false
+                    },
+                    onDismissRequest = { showCategoryDialog = false }
+                )
+            }
+
+            // Apply Filters Button
+            Button(onClick = {
+                onApplyFilter(filterOptions)
+                onDismiss()
+            }) {
+                Text("Apply Filters")
+            }
+        }
+    }
+}
+
+
 @Composable
 fun ExerciseItem(exerciseDetails: ExerciseDetails) {
     Card(
             modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp),
+                .fillMaxWidth()
+                .padding(8.dp),
     ) {
         Column(
                 modifier = Modifier
@@ -61,4 +167,64 @@ fun ExerciseItem(exerciseDetails: ExerciseDetails) {
             Text(text = "Equipment: ${exerciseDetails.equipmentList.joinToString { it.name }}")
         }
     }
+}
+
+@Composable
+fun CategorySelectionDialog(
+    categories: List<Category>?,
+    selectedCategoryId: Int?,
+    onCategorySelected: (Int) -> Unit,
+    onDismissRequest: () -> Unit
+) {
+    Dialog(onDismissRequest = onDismissRequest) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text("Select a Category", style = MaterialTheme.typography.displayLarge)
+            Spacer(modifier = Modifier.height(8.dp))
+            LazyColumn {
+                categories?.let { categoryList ->
+                    items(categoryList) { category ->
+                        Row(modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable { onCategorySelected(category.id) }
+                        .padding(8.dp)) {
+                        RadioButton(
+                            selected = category.id == selectedCategoryId,
+                            onClick = { onCategorySelected(category.id) }
+                        )
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Text(text = category.name)
+                    }
+                }
+            }
+        }
+    }
+}
+
+//object ExerciseListProvider {
+//    private val exercise1: Exercise = Exercise(nameId = "NameID", name = "Name", instructions = "Description")
+//    private val exercise2: Exercise = Exercise(nameId = "Completely Different", name = "Completely Different", instructions = "Description")
+//    private val category: Category = Category(name = "Name", description = "Description")
+//    private val equipment: Equipment = Equipment(name = "Name", description = "Description")
+//
+//    fun getSampleExercises(): List<ExerciseDetails>{
+//        val exerciseDetails = ExerciseDetails(
+//            exercise = exercise1,
+//            mutableListOf(category),
+//            mutableListOf(equipment)
+//        )
+//        val list = mutableListOf<ExerciseDetails>(
+//            ExerciseDetails(
+//                exercise = exercise1,
+//                mutableListOf(category),
+//                mutableListOf(equipment)
+//            ),
+//            ExerciseDetails(exercise = exercise2, mutableListOf(category), mutableListOf(equipment))
+//        )
+//        for (i in 1..20) {
+//            list.add(exerciseDetails)
+//        }
+//        return list
+//    }
+
+
 }
--- app/src/main/java/pro/selecto/slothos/ui/exercise/ExerciseListViewModel.kt
+++ app/src/main/java/pro/selecto/slothos/ui/exercise/ExerciseListViewModel.kt
@@ -2,29 +2,77 @@ package pro.selecto.slothos.ui.exercise
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.filterNotNull
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import pro.selecto.slothos.data.ExerciseDetails
 import pro.selecto.slothos.data.ExerciseDetailsService
+import pro.selecto.slothos.data.entities.Category
+import pro.selecto.slothos.data.entities.Equipment
+import pro.selecto.slothos.data.repositories.interfaces.CategoryRepository
+import pro.selecto.slothos.data.repositories.interfaces.EquipmentRepository
 import javax.inject.Inject
 
 class ExerciseListViewModel @Inject constructor (
-    exerciseDetailsService: ExerciseDetailsService
+    exerciseDetailsService: ExerciseDetailsService,
+    categoryRepository: CategoryRepository,
+    equipmentRepository: EquipmentRepository
 ) : ViewModel() {
-    val uiState: StateFlow<ExerciseListUiState?> =
-        exerciseDetailsService.getAllExerciseDetails()
-            .filterNotNull()
-            .map { ExerciseListUiState(exerciseDetailsList = it) }
-            .stateIn(
-                scope = viewModelScope,
-                started = SharingStarted.Lazily,
-                initialValue = null
-            )
+    private val _uiState = MutableStateFlow<ExerciseListUiState?>(null)
+    val uiState: StateFlow<ExerciseListUiState?> = _uiState.asStateFlow()
+
+    private val _filterOptions = MutableStateFlow(FilterOptions())
+    val filterOptions: StateFlow<FilterOptions> = _filterOptions.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            val exercisesFlow = exerciseDetailsService.getAllExerciseDetails().filterNotNull()
+            val categoriesFlow = categoryRepository.getAllEntitiesStream().filterNotNull()
+            val equipmentFlow = equipmentRepository.getAllEntitiesStream().filterNotNull()
+
+            combine(
+                exercisesFlow,
+                categoriesFlow,
+                equipmentFlow,
+                _filterOptions
+            ) { exercises, categories, equipment, filterOptions ->
+                val filteredExercises = if (filterOptions.category != null) {
+                    exercises.filter { exercise ->
+                        exercise.categoryList.any { category ->
+                            category.id == filterOptions.category
+                        }
+                    }
+                } else {
+                    exercises
+                }
+                ExerciseListUiState(
+                    exerciseDetailsList = filteredExercises,
+                    categoryList = categories,
+                    equipmentList = equipment
+                )
+            }.collect { combinedUiState ->
+                _uiState.value = combinedUiState
+            }
+        }
+    }
+
+    fun updateFilterOptions(newFilterOptions: FilterOptions) {
+        _filterOptions.value = newFilterOptions
+    }
 }
 
 data class ExerciseListUiState(
-    val exerciseDetailsList: List<ExerciseDetails>
+    val exerciseDetailsList: List<ExerciseDetails>,
+    val categoryList: List<Category>,
+    val equipmentList: List<Equipment>
+)
+
+
+data class FilterOptions(
+    val category: Int? = null,
+    val equipment: Int? = null,
+    val exerciseInfo: String? = null
 )
