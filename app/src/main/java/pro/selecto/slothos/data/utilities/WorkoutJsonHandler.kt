944b458bb484f0de3902d213644992970ef2eeea
shane-co <seamuss.mck@gmail.com>
2024-09-06 17:32:37 -0700

    Add basic functions

---
+++ app/src/main/java/pro/selecto/slothos/data/utilities/WorkoutJsonHandler.kt
@@ -0,0 +1,14 @@
+package pro.selecto.slothos.data.utilities
+
+import kotlinx.serialization.json.Json
+
+class WorkoutJsonHandler {
+}
+
+fun convertToJson(): Json {
+
+}
+
+fun convertFromJson(): WorkoutDetails {
+
+}
