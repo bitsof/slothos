b59192eac322d55782abd1f1d8f8ec2a551be8ee
shane-co <seamuss.mck@gmail.com>
2025-01-12 12:21:42 -0800

    Change RepetitionMeasurement to Measurement

---
+++ app/src/main/java/pro/selecto/slothos/data/entities/Measurement.kt
@@ -0,0 +1,16 @@
+package pro.selecto.slothos.data.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.serialization.Serializable
+
+@Entity(tableName = "measurements")
+@Serializable
+class Measurement (
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id") val id: Int = 0,
+    @ColumnInfo(name = "name") val name: String = "",
+    @ColumnInfo(name = "description") val description: String = "",
+) {
+}
