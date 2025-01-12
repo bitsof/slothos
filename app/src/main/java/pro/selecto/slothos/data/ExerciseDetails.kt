61e6e6299b00b98d6424cf55ace960e1b6c5d5a8
shane-co <seamuss.mck@gmail.com>
2025-01-12 12:31:02 -0800

    Make exercise details serializable

--- app/src/main/java/pro/selecto/slothos/data/ExerciseDetails.kt
+++ app/src/main/java/pro/selecto/slothos/data/ExerciseDetails.kt
@@ -1,10 +1,11 @@
 package pro.selecto.slothos.data
 
+import kotlinx.serialization.Serializable
 import pro.selecto.slothos.data.entities.Category
 import pro.selecto.slothos.data.entities.Equipment
 import pro.selecto.slothos.data.entities.Exercise
 
-
+@Serializable
 data class ExerciseDetails(
     val exercise: Exercise,
     val categoryList: List<Category>,
