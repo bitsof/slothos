f75fc52b3bdd19cab5bc9f3076f03a34ef14ea1d
shane-co <seamuss.mck@gmail.com>
2025-01-26 22:44:27 -0800

    Change routes and pass NavController to composables now requiring it

--- app/src/main/java/pro/selecto/slothos/ui/SlothosApp.kt
+++ app/src/main/java/pro/selecto/slothos/ui/SlothosApp.kt
@@ -15,6 +15,7 @@ import pro.selecto.slothos.ui.exercise.ExerciseDetailsScreen
 import pro.selecto.slothos.ui.exercise.ExerciseListMode
 import pro.selecto.slothos.ui.exercise.ExerciseListScreen
 import pro.selecto.slothos.ui.exercise.InsertExerciseScreen
+import pro.selecto.slothos.ui.workout.AddSetScreen
 import pro.selecto.slothos.ui.workout.AddWorkScreen
 import pro.selecto.slothos.ui.workout.DisplayWorkoutScreen
 import pro.selecto.slothos.ui.workout.InsertWorkoutScreen
@@ -28,7 +29,7 @@ enum class SlothosScreen(@StringRes val title: Int) {
     InsertWorkout(title = R.string.insert_workout),
     DisplayWorkout(title = R.string.display_workout),
     AddSet(title = R.string.add_set),
-    WorkoutList(title = R.string.workout_list)
+    WorkoutList(title = R.string.workout_list),
     AddWork(title = R.string.add_work)
 }
 
@@ -47,7 +48,6 @@ fun SlothosApp(
                 onButtonClicked = { navController.navigate(SlothosScreen.ExerciseList.name) },
                 onButtonClicked2 = { navController.navigate(SlothosScreen.InsertExercise.name) },
                 onButtonClicked3 = { navController.navigate(SlothosScreen.InsertWorkout.name)},
-                onButtonClicked4 = { navController.navigate(SlothosScreen.DisplayWorkout.name)},
                 onButtonClicked5 = { navController.navigate(SlothosScreen.WorkoutList.name)},
                 )
         }
@@ -84,17 +84,30 @@ fun SlothosApp(
             )
         }
         composable(route = SlothosScreen.InsertExercise.name){
-            InsertExerciseScreen(viewModelFactory = viewModelFactory)
+            InsertExerciseScreen(
+                viewModelFactory = viewModelFactory,
+                navController = navController,
+            )
         }
         composable(route = SlothosScreen.InsertWorkout.name){
             InsertWorkoutScreen(
                 viewModelFactory = viewModelFactory,
-                navigateToAddSetScreen = { navController.navigate(SlothosScreen.AddSet.name)}
                 navigateToAddSetScreen = { navController.navigate(SlothosScreen.AddSet.name)},
                 navController = navController,
             )
         }
         
+        composable(route = SlothosScreen.AddSet.name) {
+            AddSetScreen(
+                navController = navController,
+                viewModelFactory = viewModelFactory,
+                onSetAdded = { setDetails ->
+                    navController.previousBackStackEntry
+                        ?.savedStateHandle
+                        ?.set("newSetDetails", setDetails)
+                    navController.popBackStack()
+                },
+                navigateToAddWorkScreen = { navController.navigate(SlothosScreen.AddWork.name) },
                 navigateToSelectExerciseScreen = { navController.navigate(SlothosScreen.ExerciseList.name + "?mode=select") },
             )
         }
@@ -111,8 +124,27 @@ fun SlothosApp(
                 }
             )
         }
+        
+        composable(
+            route = "${SlothosScreen.DisplayWorkout.name}/{workoutId}",
+            arguments = listOf(navArgument("workoutId") { type = NavType.IntType })
+        ){ backStackEntry ->
+            val workoutId = backStackEntry.arguments?.getInt("workoutId")
+            if (workoutId != null) {
+                DisplayWorkoutScreen(
+                    viewModelFactory = viewModelFactory,
+                    workoutId = workoutId,
+                )
+            }
+        }
+
         composable(route = SlothosScreen.WorkoutList.name){
-            WorkoutListScreen(viewModelFactory = viewModelFactory)
+            WorkoutListScreen(
+                viewModelFactory = viewModelFactory,
+                onWorkoutClick = { workoutDetails ->
+                    navController.navigate("${SlothosScreen.DisplayWorkout}/${workoutDetails.workout.id}")
+                },
+            )
         }
         composable(
             route = "${SlothosScreen.ExerciseDetails}/{exerciseId}",
