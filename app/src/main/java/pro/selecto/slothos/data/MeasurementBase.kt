5182e7e0d94fcd4f765e28921a779f8428a0f760
shane-mckenzie <seamuss.mck@gmail.com>
2025-01-26 23:01:32 -0800

    Re-Add Measurement Base (#26)
    * Add add work screen and view model
    
    * Improve layout and add navigation back to insert exercise screen
    
    * Improve layout of home page
    
    * Implement AddSetScreen and AddSetViewModel
    
    * Add parcelize to build file
    
    * Make classes parcelable
    
    * Add modes to ExerciseListScreen
    
    * Fix "Id" misspelling
    
    * Give exercise details default values
    
    * Add savedStateHandle DI
    
    * Make workout parcelable
    
    * Set default value for WorkoutDetails
    
    * Change to uiState instead of individual mutable fields
    
    * Add savedstate to injected for maintaining state when navigating away
    
    * Set nullable fields to null on default to prevent foreign key field error
    
    * Add navigation to setscreen and popping back on nav stack to InsertWorkoutScreen
    
    * Add standard measurements as enums
    
    * Change routes and pass NavController to composables now requiring it
    
    * Add new view models
    
    * Remove measurement parameter
    
    * Increment database version
    
    * Change optin
    
    * Change optin
    
    * Add navigating to WorkoutDetails from WorkoutList
    
    * Readd measurement base

---
+++ app/src/main/java/pro/selecto/slothos/data/MeasurementBase.kt
@@ -0,0 +1,13 @@
+package pro.selecto.slothos.data
+
+import java.math.BigDecimal
+
+interface MeasurementBase {
+    val id: Int
+    val name: String
+    val description: String
+    val symbol: String
+    val conversionToBase: BigDecimal
+    val categoryType: MeasurementCategory
+    val defaultPrecision: Int
+}
