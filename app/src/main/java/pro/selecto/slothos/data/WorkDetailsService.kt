c8ff24492d100751bd8b9a3e1cf7c569425b2a03
shane-co <seamuss.mck@gmail.com>
2025-01-26 22:45:36 -0800

    Remove measurement parameter

--- app/src/main/java/pro/selecto/slothos/data/WorkDetailsService.kt
+++ app/src/main/java/pro/selecto/slothos/data/WorkDetailsService.kt
@@ -34,7 +34,6 @@ class WorkDetailsService @Inject constructor(
                     }
                     WorkDetails(
                         work = work,
-                        measurement = measurement
                     )
                 }
             }
