85f7e7f213b64e0b7209da55f4675995c4a48b6f
shane-co <seamuss.mck@gmail.com>
2025-01-26 20:12:50 -0800

    Improve layout of home page

--- app/src/main/java/pro/selecto/slothos/ui/HomeScreen.kt
+++ app/src/main/java/pro/selecto/slothos/ui/HomeScreen.kt
@@ -1,24 +1,25 @@
 package pro.selecto.slothos.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeScreen(
     onButtonClicked: () -> Unit,
     onButtonClicked2: () -> Unit,
     onButtonClicked3: () -> Unit,
-    onButtonClicked4: () -> Unit,
     onButtonClicked5: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Column() {
+    Column(modifier = modifier.padding(16.dp)) {
         Text(
-            text = "Checking",
+            text = "Home Page",
             style = MaterialTheme.typography.headlineSmall,
         )
         Button(
@@ -39,15 +40,9 @@ fun HomeScreen(
         }
 
         Button(
-            onClick = onButtonClicked4,
-        ) {
-            Text("Display Workout")
-        }
-
-        Button(
             onClick = onButtonClicked5,
         ) {
-            Text("Workout Lis")
+            Text("Workout List")
         }
     }
 }
